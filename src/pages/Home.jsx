a99b5100b9bdbffa3af590b669015e217ee5f4a4
Bisente <vincentcuevas0623@gmail.com>
2025-04-25 03:26:29 +0800

    adjusted home width on mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -79,7 +79,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-8 max-w-screen">
         <Background />
         <HeroSection />
         <TeamIntroduction />
